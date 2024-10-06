2a34a5691e8b1e51d9ee825e387303fd54b39d40
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-06 19:23:20 +0530

    project complete, generating cards with dynamic data

--- pokedex/script.js
+++ pokedex/script.js
@@ -1,7 +1,7 @@
-const poke_container = document.getAnimations("poke-container");
+const poke_container = document.getElementById("poke-container");
 const pokemon_count = 150;
 
-const color = {
+const colors = {
   fire: "#FDDFDF",
   grass: "#DEFDE0",
   electric: "#FCF7DE",
@@ -18,6 +18,8 @@ const color = {
   normal: "#f5f5f5",
 };
 
+const main_types = Object.keys(colors);
+
 const fetchPokemon = async () => {
   for (let i = 1; i <= pokemon_count; i++) {
     await getPokemon(i);
@@ -29,7 +31,40 @@ const getPokemon = async (id) => {
   const res = await fetch(url);
 
   const data = await res.json();
-  console.log(data);
+  createPokemonCard(data);
+};
+
+const createPokemonCard = (pokemon) => {
+  // for each pokemon, we fetch we'll create a card as designed in the HTML file.
+  // for production grade app or for large data, below approach would have benn different
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
+
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const id = pokemon.id.toString().padStart(3, "0");
+  const poke_types = pokemon.types.map((type) => type.type.name);
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+  const color = colors[type];
+
+  pokemonEl.style.backgroundColor = color;
+
+  const pokemonInnerHTML = `
+        <div class="img-container">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
+            alt=""
+          />
+        </div>
+        <div class="info">
+          <span class="number">#${id}</span>
+          <h3 class="name">${name}</h3>
+          <small class="type">Type: <span>${type}</span></small>
+        </div>
+      </div>
+    `;
+
+  pokemonEl.innerHTML = pokemonInnerHTML;
+  poke_container.appendChild(pokemonEl);
 };
 
 fetchPokemon();
